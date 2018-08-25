f8450857309afac428e61bad1d12c3db7e1e1859
walmir <walmir.rocha@gmail.com>
2018-08-25 19:52:29 -0300

    First commit

---
+++ config-client/src/main/java/br/com/wlr/configclient/ConfigClientApplication.java
@@ -0,0 +1,14 @@
+package br.com.wlr.configclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@EnableWebSecurity
+@SpringBootApplication
+public class ConfigClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigClientApplication.class, args);
+	}
+}
