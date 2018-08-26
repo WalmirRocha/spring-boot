479736379cdded02b8b9ff7d646423b9363c74d0
walmir.rocha <walmir.rocha@gmail.com>
2018-08-26 10:54:24 -0300

    removed files

--- config-client/src/main/java/br/com/wlr/configclient/Greeter.java
+++ config-client/src/main/java/br/com/wlr/configclient/Greeter.java
@@ -14,22 +14,22 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Greeter {
 
-    @Value("${message.greeting}")
-    String greeting;
+	 @Value("${message.greeting:walmirl}")
+	    String greeting;
 
-    @Value("${server.port}")
-    int port;
+	    @Value("${server.port:8080}")
+	    int port;
 
-    @Value("${configuration.projectName}")
-    String projectName;
+	    @Value("${configuration.projectName:local}")
+	    String projectName;
 
-    @RequestMapping(value = "/", produces = "application/json")
-    public List<String> index(){
-        List<String> env = Arrays.asList(
-                "message.greeting is: " + greeting,
-                "server.port is: " + port,
-                "configuration.projectName is: " + projectName
-        );
-        return env;
-    }
+	    @RequestMapping(value = "/", produces = "application/json")
+	    public List<String> index(){
+	        List<String> env = Arrays.asList(
+	                "message.greeting is: " + greeting,
+	                "server.port is: " + port,
+	                "configuration.projectName is: " + projectName
+	        );
+	        return env;
+	    }
 }
