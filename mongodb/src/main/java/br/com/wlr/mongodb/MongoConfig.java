344a8fd3e2cf408a656977abdf004ed19f2fb855
walmir.rocha <walmir.rocha@gmail.com>
2018-08-29 21:17:23 -0300

    Adding configuration for conection in MongoDB

--- mongodb/src/main/java/br/com/wlr/mongodb/MongoConfig.java
+++ mongodb/src/main/java/br/com/wlr/mongodb/MongoConfig.java
@@ -1,17 +1,15 @@
 package br.com.wlr.mongodb;
 
-import java.util.Arrays;
-
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.config.AbstractMongoConfiguration;
 import org.springframework.data.mongodb.core.MongoTemplate;
-import org.springframework.data.mongodb.core.mapping.MongoMappingContext;
 
 import com.mongodb.MongoClient;
+import com.mongodb.MongoClientOptions;
+import com.mongodb.MongoClientURI;
 import com.mongodb.MongoCredential;
-import com.mongodb.ServerAddress;
 
 @Configuration
 public class MongoConfig extends AbstractMongoConfiguration {
@@ -46,7 +44,9 @@ public class MongoConfig extends AbstractMongoConfiguration {
 	@Override
 	@Bean
 	public MongoClient mongoClient() {
-        MongoClient mongoClient = new MongoClient(new ServerAddress(mongoHost, mongoPort), Arrays.asList(MongoCredential.createCredential(username, mongoDB, password.toCharArray())));
-		return mongoClient;
+		MongoClientOptions mongoClientOptions = new MongoClientOptions.Builder().connectTimeout(30000).build();
+		MongoCredential credential = MongoCredential.createCredential(username, mongoDB, password.toCharArray());
+		MongoClientURI clientURI = new MongoClientURI("mongodb://admin:admin@localhost:27017/?authSource=admin");
+		return new MongoClient(clientURI);
 	}
 }
